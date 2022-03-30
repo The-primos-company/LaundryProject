6a5502087b9a980f596d886de0bf6084069f6b30
tanuki <danfeedev@gmail.com>
2022-03-29 23:42:07 -0500

    add garments

--- frontend/src/wailsjs/go/models.ts
+++ frontend/src/wailsjs/go/models.ts
@@ -2,11 +2,40 @@
 
 export {};
 
+export class Garment {
+    id: number[];
+    order_id: number[];
+    total: string;
+    category: string;
+    gendre: string;
+    color: string;
+    brand: string;
+    price: string;
+    comment: string;
+    defects: string;
+
+    static createFrom(source: any = {}) {
+        return new Garment(source);
+    }
+
+    constructor(source: any = {}) {
+        if ('string' === typeof source) source = JSON.parse(source);
+        this.id = source["id"];
+        this.order_id = source["order_id"];
+        this.total = source["total"];
+        this.category = source["category"];
+        this.gendre = source["gendre"];
+        this.color = source["color"];
+        this.brand = source["brand"];
+        this.price = source["price"];
+        this.comment = source["comment"];
+        this.defects = source["defects"];
+    }
+}
 export class Order {
     ID: number[];
     recieved_date: string;
     delivery_date: string;
-    comments: string;
     client_name: string;
     client_id: string;
     client_address: string;
@@ -16,6 +45,7 @@ export class Order {
     payment_total_payed: string;
     payment_total: string;
     payment_total_real: string;
+    garments: Garment[];
 
     static createFrom(source: any = {}) {
         return new Order(source);
@@ -26,7 +56,6 @@ export class Order {
         this.ID = source["ID"];
         this.recieved_date = source["recieved_date"];
         this.delivery_date = source["delivery_date"];
-        this.comments = source["comments"];
         this.client_name = source["client_name"];
         this.client_id = source["client_id"];
         this.client_address = source["client_address"];
@@ -36,5 +65,24 @@ export class Order {
         this.payment_total_payed = source["payment_total_payed"];
         this.payment_total = source["payment_total"];
         this.payment_total_real = source["payment_total_real"];
+        this.garments = this.convertValues(source["garments"], Garment);
     }
+
+	convertValues(a: any, classs: any, asMap: boolean = false): any {
+	    if (!a) {
+	        return a;
+	    }
+	    if (a.slice) {
+	        return (a as any[]).map(elem => this.convertValues(elem, classs));
+	    } else if ("object" === typeof a) {
+	        if (asMap) {
+	            for (const key of Object.keys(a)) {
+	                a[key] = new classs(a[key]);
+	            }
+	            return a;
+	        }
+	        return new classs(a);
+	    }
+	    return a;
+	}
 }
