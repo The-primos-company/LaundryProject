e010f048648df691f8b868bba6d21ac1000ac083
Feli <srgatoiscool@gmail.com>
2022-03-31 13:38:02 -0500

    order

--- frontend/src/components/PrintOrder/PrintOrder.jsx
+++ frontend/src/components/PrintOrder/PrintOrder.jsx
@@ -13,20 +13,11 @@ export const PrintOrder = ({ order, orderNumber }) => {
   return (
     <div>
       <button onClick={handlePrint}>Print this out!</button>
-      <div className="print-container" style={{ margin: "0", padding: "0" }}>
-        <ComponentToPrint
-          ref={componentRef}
-          order={order}
-          orderNumber={orderNumber}
-        />
-      </div>
-      <div className="print-container" style={{ margin: "0", padding: "0" }}>
-        <ComponentToPrint
-          ref={componentRef}
-          order={order}
-          orderNumber={orderNumber}
-        />
-      </div>
+      <ComponentToPrint
+        ref={componentRef}
+        order={order}
+        orderNumber={orderNumber}
+      />
     </div>
   );
 };
@@ -36,148 +27,292 @@ const ComponentToPrint = forwardRef((props, ref) => {
   const { order, orderNumber } = props;
   return (
     <>
-      <div className="page-break" />
-      <table ref={ref}>
-        <thead>
-          <tr>
-            <th>Lava super</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Orden de servicio </td>
-            <td></td>
-            <td>{orderNumber}</td>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>Fecha de ingreso</td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>Cliente</td>
-            <td>{order.client_name}</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>Cedula</td>
-            <td>{order.client_id}</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>Direccion</td>
-            <td>{order.client_address}</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>Telefono</td>
-            <td>{order.client_phone}</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>Email</td>
-            <td>{order.client_email}</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>cantidad</td>
-            <td>Descripcion</td>
-            <td>Valor</td>
-          </tr>
-          {order.garments.map((item) => {
-            return (
-              <tr>
-                <td>{item.cuantity}</td>
-                {/* <td>Camisa masculino roja gucci </td> */}
-                <td>
-                  {item.category} {item.gendre} {item.color} {item.brand}
-                </td>
-                <td>{item.price}</td>
-              </tr>
-            );
-          })}
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>Total:</td>
-            <td>{order.payment_total}</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>Abono:</td>
-            <td>{order.payment_total_payed}</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>Saldo:</td>
-            <td>
-              {order.payment_total} - {order.payment_total_payed}
-            </td>
-            <td></td>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>Entrega Aprox</td>
-            <td>{order.delivery_date}</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>Observaciones:</td>
-            <td></td>
-          </tr>
-          {order.garments.map((item) => {
-            return (
-              <tr>
-                <td colSpan={3}>
-                  {item.category} {item.gendre} {item.color} {item.brand}:{" "}
-                  {item.comment} {item.defects}
-                </td>
-              </tr>
-            );
-          })}
-          <tr>
-            <td>Nota</td>
-            <td>
-              LAVA-ROPAS no se responsabiliza por botones, hebillas, adornos, ni
-              por objetos o dinero dejados en las prendas.
-            </td>
-            <td>
-              LAVA-ROPAS no se responsabiliza por las variaciones que se
-              produzcan en el color
-            </td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="print-container" style={{ margin: "0", padding: "0" }}>
+        <div className="page-break" />
+        <table ref={ref}>
+          <thead>
+            <tr>
+              <th>Lava super</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Orden de servicio </td>
+              <td></td>
+              <td>{orderNumber}</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Fecha de ingreso</td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Cliente</td>
+              <td>{order.client_name}</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Cedula</td>
+              <td>{order.client_id}</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Direccion</td>
+              <td>{order.client_address}</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Telefono</td>
+              <td>{order.client_phone}</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Email</td>
+              <td>{order.client_email}</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>cantidad</td>
+              <td>Descripcion</td>
+              <td>Valor</td>
+            </tr>
+            {order.garments.map((item) => {
+              return (
+                <tr>
+                  <td>{item.cuantity}</td>
+                  {/* <td>Camisa masculino roja gucci </td> */}
+                  <td>
+                    {item.category} {item.gendre} {item.color} {item.brand}
+                  </td>
+                  <td>{item.price}</td>
+                </tr>
+              );
+            })}
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Total:</td>
+              <td>{order.payment_total}</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Abono:</td>
+              <td>{order.payment_total_payed}</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Saldo:</td>
+              <td>
+                {order.payment_total} - {order.payment_total_payed}
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Entrega Aprox</td>
+              <td>{order.delivery_date}</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Observaciones:</td>
+              <td></td>
+            </tr>
+            {order.garments.map((item) => {
+              return (
+                <tr>
+                  <td colSpan={3}>
+                    {item.category} {item.gendre} {item.color} {item.brand}:{" "}
+                    {item.comment} {item.defects}
+                  </td>
+                </tr>
+              );
+            })}
+            <tr>
+              <td>Nota</td>
+              <td>
+                LAVA-ROPAS no se responsabiliza por botones, hebillas, adornos,
+                ni por objetos o dinero dejados en las prendas.
+              </td>
+              <td>
+                LAVA-ROPAS no se responsabiliza por las variaciones que se
+                produzcan en el color
+              </td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="page-break" />
+        <table ref={ref}>
+          <thead>
+            <tr>
+              <th>Lava super</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Orden de servicio </td>
+              <td></td>
+              <td>{orderNumber}</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Fecha de ingreso</td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Cliente</td>
+              <td>{order.client_name}</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Cedula</td>
+              <td>{order.client_id}</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Direccion</td>
+              <td>{order.client_address}</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Telefono</td>
+              <td>{order.client_phone}</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Email</td>
+              <td>{order.client_email}</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>cantidad</td>
+              <td>Descripcion</td>
+              <td>Valor</td>
+            </tr>
+            {order.garments.map((item) => {
+              return (
+                <tr>
+                  <td>{item.cuantity}</td>
+                  {/* <td>Camisa masculino roja gucci </td> */}
+                  <td>
+                    {item.category} {item.gendre} {item.color} {item.brand}
+                  </td>
+                  <td>{item.price}</td>
+                </tr>
+              );
+            })}
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Total:</td>
+              <td>{order.payment_total}</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Abono:</td>
+              <td>{order.payment_total_payed}</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Saldo:</td>
+              <td>
+                ({order.payment_total} - {order.payment_total_payed})
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Entrega Aprox</td>
+              <td>{order.delivery_date}</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Observaciones:</td>
+              <td></td>
+            </tr>
+            {order.garments.map((item) => {
+              return (
+                <tr>
+                  <td colSpan={3}>
+                    {item.category} {item.gendre} {item.color} {item.brand}:{" "}
+                    {item.comment} {item.defects}
+                  </td>
+                </tr>
+              );
+            })}
+            <tr>
+              <td>Nota</td>
+              <td>
+                LAVA-ROPAS no se responsabiliza por botones, hebillas, adornos,
+                ni por objetos o dinero dejados en las prendas.
+              </td>
+              <td>
+                LAVA-ROPAS no se responsabiliza por las variaciones que se
+                produzcan en el color
+              </td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 });
