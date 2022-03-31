c9c9b664c001a06668fede721f361c7af387b292
Feli <srgatoiscool@gmail.com>
2022-03-31 15:15:36 -0500

    ola

--- frontend/src/components/PrintOrder/PrintOrder.jsx
+++ frontend/src/components/PrintOrder/PrintOrder.jsx
@@ -177,79 +177,84 @@ const ComponentToPrint = forwardRef((props, ref) => {
             <td></td>
           </tbody>
           {/* Owner */}
-          <div className="page-break" />
-          <thead>
-            <tr>
-              <th colSpan={3}>Lava super</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td colSpan={2}>Orden de servicio </td>
-              <td>50</td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>cantidad</td>
-              <td colSpan={2}>Descripcion</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td colSpan={2}>Camisa masculino roja gucci</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td colSpan={2}>Camisa masculino azul gucci</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td colSpan={2}>Camisa masculino verde gucci</td>
-            </tr>
-            <tr>
-              <td colSpan={2}></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Total:</td>
-              <td>30,000</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Abono:</td>
-              <td>20,000</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Saldo:</td>
-              <td>10,000</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Entrega Aprox</td>
-              <td colSpan={2}>2022-03-17 6:30pm</td>
-            </tr>
-            <tr>
-              <td colSpan={3}>Observaciones:</td>
-            </tr>
-            <tr>
-              <td colSpan={3}>Camisa masculino roja gucci - picado-moteado</td>
-            </tr>
-            <tr>
-              <td colSpan={3}>Camisa masculino verde gucci - picado</td>
-            </tr>
-            <tr>
-              <td colSpan={3}>Camisa masculino azul gucci - decolorado</td>
-            </tr>
-          </tbody>
+          <div className="page-break">
+            <table>
+              <thead>
+                <tr>
+                  <th colSpan={3}>Lava super</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td colSpan={2}>Orden de servicio </td>
+                  <td>50</td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>cantidad</td>
+                  <td colSpan={2}>Descripcion</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td colSpan={2}>Camisa masculino roja gucci</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td colSpan={2}>Camisa masculino azul gucci</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td colSpan={2}>Camisa masculino verde gucci</td>
+                </tr>
+                <tr>
+                  <td colSpan={2}></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>Total:</td>
+                  <td>30,000</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>Abono:</td>
+                  <td>20,000</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>Saldo:</td>
+                  <td>10,000</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>Entrega Aprox</td>
+                  <td colSpan={2}>2022-03-17 6:30pm</td>
+                </tr>
+                <tr>
+                  <td colSpan={3}>Observaciones:</td>
+                </tr>
+                <tr>
+                  <td colSpan={3}>
+                    Camisa masculino roja gucci - picado-moteado
+                  </td>
+                </tr>
+                <tr>
+                  <td colSpan={3}>Camisa masculino verde gucci - picado</td>
+                </tr>
+                <tr>
+                  <td colSpan={3}>Camisa masculino azul gucci - decolorado</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </table>
         {/* Owner */}
       </div>
