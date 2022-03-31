a1d74e1e63d825a7b2998ec25fcce311a3435e31
Feli <srgatoiscool@gmail.com>
2022-03-31 13:45:01 -0500

    date

--- frontend/src/components/PrintOrder/PrintOrder.jsx
+++ frontend/src/components/PrintOrder/PrintOrder.jsx
@@ -4,7 +4,7 @@ import { useReactToPrint } from "react-to-print";
 import "./PrintOrder.css";
 
 export const PrintOrder = ({ order, orderNumber }) => {
-  console.log("Order =>", order);
+  console.log("Order =>", orderNumber);
   const componentRef = useRef();
   const handlePrint = useReactToPrint({
     content: () => componentRef.current,
@@ -25,6 +25,7 @@ export const PrintOrder = ({ order, orderNumber }) => {
 const ComponentToPrint = forwardRef((props, ref) => {
   console.log("Print =>", props);
   const { order, orderNumber } = props;
+  let total = order.payment_total - order.payment_total_payed;
   return (
     <>
       <div className="print-container" style={{ margin: "0", padding: "0" }}>
@@ -122,9 +123,7 @@ const ComponentToPrint = forwardRef((props, ref) => {
             </tr>
             <tr>
               <td>Saldo:</td>
-              <td>
-                {order.payment_total} - {order.payment_total_payed}
-              </td>
+              <td>{total}</td>
               <td></td>
             </tr>
             <tr>
@@ -134,7 +133,7 @@ const ComponentToPrint = forwardRef((props, ref) => {
             </tr>
             <tr>
               <td>Entrega Aprox</td>
-              <td>{order.delivery_date}</td>
+              <td>{order.delivery_date.toDateString()}</td>
               <td></td>
             </tr>
             <tr>
@@ -158,156 +157,18 @@ const ComponentToPrint = forwardRef((props, ref) => {
             })}
             <tr>
               <td>Nota</td>
-              <td>
-                LAVA-ROPAS no se responsabiliza por botones, hebillas, adornos,
-                ni por objetos o dinero dejados en las prendas.
-              </td>
-              <td>
-                LAVA-ROPAS no se responsabiliza por las variaciones que se
-                produzcan en el color
-              </td>
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
-        <div className="page-break" />
-        <table ref={ref}>
-          <thead>
-            <tr>
-              <th>Lava super</th>
-              <th></th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Orden de servicio </td>
-              <td></td>
-              <td>{orderNumber}</td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Fecha de ingreso</td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Cliente</td>
-              <td>{order.client_name}</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Cedula</td>
-              <td>{order.client_id}</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Direccion</td>
-              <td>{order.client_address}</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Telefono</td>
-              <td>{order.client_phone}</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Email</td>
-              <td>{order.client_email}</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>cantidad</td>
-              <td>Descripcion</td>
-              <td>Valor</td>
-            </tr>
-            {order.garments.map((item) => {
-              return (
-                <tr>
-                  <td>{item.cuantity}</td>
-                  {/* <td>Camisa masculino roja gucci </td> */}
-                  <td>
-                    {item.category} {item.gendre} {item.color} {item.brand}
-                  </td>
-                  <td>{item.price}</td>
-                </tr>
-              );
-            })}
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Total:</td>
-              <td>{order.payment_total}</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Abono:</td>
-              <td>{order.payment_total_payed}</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Saldo:</td>
-              <td>
-                ({order.payment_total} - {order.payment_total_payed})
-              </td>
-              <td></td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Entrega Aprox</td>
-              <td>{order.delivery_date}</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Observaciones:</td>
-              <td></td>
-            </tr>
-            {order.garments.map((item) => {
-              return (
-                <tr>
-                  <td colSpan={3}>
-                    {item.category} {item.gendre} {item.color} {item.brand}:{" "}
-                    {item.comment} {item.defects}
-                  </td>
-                </tr>
-              );
-            })}
-            <tr>
-              <td>Nota</td>
-              <td>
-                LAVA-ROPAS no se responsabiliza por botones, hebillas, adornos,
-                ni por objetos o dinero dejados en las prendas.
-              </td>
-              <td>
-                LAVA-ROPAS no se responsabiliza por las variaciones que se
-                produzcan en el color
-              </td>
+              <tr>
+                <td>
+                  LAVA-ROPAS no se responsabiliza por botones, hebillas,
+                  adornos, ni por objetos o dinero dejados en las prendas.
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  LAVA-ROPAS no se responsabiliza por las variaciones que se
+                  produzcan en el color
+                </td>
+              </tr>
               <td></td>
             </tr>
           </tbody>
