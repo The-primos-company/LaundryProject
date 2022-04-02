c5f6ad55e11c231859747b995ea21daf6828ee14
Feli <srgatoiscool@gmail.com>
2022-04-02 14:58:04 -0500

    new changes

---
+++ frontend/src/pages/Orders/Orders.jsx
@@ -0,0 +1,10 @@
+import { Container } from "@mui/material";
+import React from "react";
+
+export const Orders = () => {
+  return (
+    <Container>
+      <h1>Order</h1>
+    </Container>
+  );
+};
