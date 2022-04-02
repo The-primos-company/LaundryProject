d5e8c9b839c2c89fb7f8a0963d8c7895201dd5a9
Feli <srgatoiscool@gmail.com>
2022-04-02 13:46:34 -0500

    ticket

--- frontend/src/components/PrintOrder/PrintOrder.jsx
+++ frontend/src/components/PrintOrder/PrintOrder.jsx
@@ -33,228 +33,186 @@ const ComponentToPrint = forwardRef((props, ref) => {
   const { order, orderNumber } = props;
 
   return (
-    <div ref={ref}>
+    <Container ref={ref}>
       <div className="page-break">
         {/* Compra */}
-        <table>
-          <div className="">
-            <img src={logo} alt="" />
-          </div>
+        <Stack direction={"row"} justifyContent="center">
+          <img src={logo} alt="" />
+        </Stack>
+        <Stack
+          sx={{ marginTop: 5, marginBottom: 2 }}
+          direction={"row"}
+          justifyContent="space-between"
+        >
+          <span>Orden de servicio</span>
+          <strong>{orderNumber}</strong>
+        </Stack>
+
+        <Stack direction={"column"} sx={{ marginBottom: 2 }}>
+          <span>
+            Fecha de ingreso{" "}
+            <strong>
+              {moment(order.recieved_date).format("MMMM d YYYY, h:mm a")}
+            </strong>
+          </span>
+
+          <span>
+            Cliente: <strong>{order.client_name}</strong>
+          </span>
+          <span>
+            Cedula: <strong>{order.client_id}</strong>
+          </span>
+          <span>Direccion:</span>
+          <span>{order.client_address}</span>
+          <span>Telefono:</span>
+          <span>{order.client_phone}</span>
+          <span>Email:</span>
+          <span>{order.client_email}</span>
+        </Stack>
+
+        <table style={{ marginBottom: 10 }}>
           <tbody>
             <tr>
-              <td colSpan={2}>Orden de servicio </td>
-              <td>{orderNumber}</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Fecha de ingreso</td>
-              <td>
-                {moment(order.recieved_date).format("MMMM Do YYYY, h:mm a")}
+              <td className="border">
+                <strong>Cantidad</strong>
               </td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Cliente</td>
-              <td>{order.client_name}</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Cedula</td>
-              <td>{order.client_id}</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Direccion</td>
-              <td>{order.client_address}</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Telefono</td>
-              <td>{order.client_phone}</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Email</td>
-              <td>{order.client_email}</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>cantidad</td>
-              <td>Descripcion</td>
-              <td>Valor</td>
+              <td className="border">
+                <strong>Descripcion</strong>
+              </td>
+              <td className="border">
+                <strong>Valor</strong>
+              </td>
             </tr>
             {order.garments.map((item) => {
               return (
                 <tr>
-                  <td>{item.cuantity}</td>
+                  <td className="border">{item.cuantity}</td>
                   {/* <td>Camisa masculino roja gucci </td> */}
-                  <td>
+                  <td className="border">
                     {item.category} {item.gendre} {item.color} {item.brand}
                   </td>
-                  <td>${parseInt(item.cuantity) * parseInt(item.price)}</td>
-                </tr>
-              );
-            })}
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Total Prendas:</td>
-              <td>{order.garment_total}</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Total:</td>
-              <td>{order.payment_total}</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Abono:</td>
-              <td>{order.payment_total_payed}</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Saldo:</td>
-              <td>{order.payment_total_real}</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Entrega Aprox</td>
-              <td>
-                {moment(order.delivery_date).format("MMMM Do YYYY, h:mm a")}
-              </td>
-              <td></td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>Observaciones:</td>
-              <td></td>
-            </tr>
-            {order.garments.map((item) => {
-              if (item.comment == "" && item.defects == "") return <tr></tr>;
-              return (
-                <tr>
-                  <td colSpan={3}>
-                    {item.category} {item.gendre} {item.color} {item.brand}:{" "}
-                    {item.comment} {item.defects}
+                  <td className="border">
+                    ${parseInt(item.cuantity) * parseInt(item.price)}
                   </td>
                 </tr>
               );
             })}
-            <tr>
-              <td>Nota:</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td colSpan={3}>
-                LAVA-SUR no se responsabiliza por botones, hebillas, adornos, ni
-                por objetos o dinero dejados en las prendas.
-              </td>
-            </tr>
-            <tr>
-              <td colSpan={3}>
-                LAVA-SUR no se responsabiliza por las variaciones que se
-                produzcan en el color
-              </td>
-            </tr>
-            <td></td>
           </tbody>
         </table>
-      </div>
-      <div className="page-break">
-        {/* Owner */}
-        <Container>
-          <Stack direction={"row"} justifyContent="center">
-            <img src={logo} alt="" />
-          </Stack>
-          <Stack>
-            <strong>Cra 122 # 122 - 16A - 18 Alto Pance</strong>
-            <strong>Teléfono fijo: 3715499</strong>
-            <strong>Celular: 315 2479406</strong>
-          </Stack>
-          <Stack
-            sx={{ marginTop: 5, marginBottom: 2 }}
-            direction={"row"}
-            justifyContent="space-between"
-          >
-            <span>Orden de servicio</span>
-            <strong>{orderNumber}</strong>
-          </Stack>
-          <Stack>
-            <table>
-              <tbody>
-                <tr>
-                  <td>
-                    <strong>Cantidad</strong>
-                  </td>
-                  <td>
-                    <strong>Descripcion</strong>
-                  </td>
-                </tr>
-                {order.garments.map((item) => {
-                  return (
-                    <tr>
-                      <td style={{ textAlign: "center" }}>{item.cuantity}</td>
-                      <td>
-                        {item.category} {item.gendre} {item.color} {item.brand}
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </Stack>
-          <Stack
-            direction={"row"}
-            justifyContent="space-between"
-            sx={{ marginTop: 2, marginBottom: 1 }}
-          >
-            <strong>Entrega Aprox</strong>
-            <span>
+        <Stack
+          direction={"column"}
+          justifyContent="space-between"
+          sx={{ marginBottom: 2 }}
+        >
+          <span>Total Prendas: {order.garment_total}</span>
+          <span>Total: {order.payment_total}</span>
+          <span>Abono: {order.payment_total_payed}</span>
+          <span>
+            Saldo: <strong>{order.payment_total_real}</strong>
+          </span>
+          <span>
+            Entrega Aprox:
+            <strong>
+              {" "}
               {moment(order.delivery_date).format("MMMM d YYYY, h:mm a")}
-            </span>
-          </Stack>
-          <Stack direction={"column"} justifyContent="space-between">
-            {" "}
-            <strong>Observaciones:</strong>
-            {order.garments.map((item) => {
-              if (item.comment === "" && item.defects === "") return <></>;
-              return (
+            </strong>
+          </span>
+        </Stack>
+        <Stack direction={"column"} justifyContent="space-between">
+          {" "}
+          <strong>Observaciones:</strong>
+          {order.garments.map((item) => {
+            if (item.comment === "" && item.defects === "") return <></>;
+            return (
+              <>
                 <span style={{ marginBottom: 5 }}>
                   {item.category} {item.gendre} {item.color} {item.brand}:{" "}
                   {item.comment} {item.defects}
                 </span>
-              );
-            })}
-          </Stack>
-        </Container>
+              </>
+            );
+          })}
+        </Stack>
+        <Stack>
+          <strong>Nota:</strong>
+          <span>
+            LAVA-SUR no se responsabiliza por botones, hebillas, adornos, ni por
+            objetos o dinero dejados en las prendas.
+          </span>
+          <span>
+            LAVA-SUR no se responsabiliza por las variaciones que se produzcan
+            en el color
+          </span>
+        </Stack>
       </div>
-    </div>
+      <div className="page-break">
+        {/* Owner */}
+        <Stack direction={"row"} justifyContent="center">
+          <img src={logo} alt="" />
+        </Stack>
+        <Stack>
+          <strong>Cra 122 # 122 - 16A - 18 Alto Pance</strong>
+          <strong>Teléfono fijo: 3715499</strong>
+          <strong>Celular: 315 2479406</strong>
+        </Stack>
+        <Stack
+          sx={{ marginTop: 5, marginBottom: 2 }}
+          direction={"row"}
+          justifyContent="space-between"
+        >
+          <span>Orden de servicio</span>
+          <strong>{orderNumber}</strong>
+        </Stack>
+        <Stack>
+          <table>
+            <tbody>
+              <tr>
+                <td className="border">
+                  <strong>Cantidad</strong>
+                </td>
+                <td className="border">
+                  <strong>Descripcion</strong>
+                </td>
+              </tr>
+              {order.garments.map((item) => {
+                return (
+                  <tr>
+                    <td className="border" style={{ textAlign: "center" }}>
+                      {item.cuantity}
+                    </td>
+                    <td className="border">
+                      {item.category} {item.gendre} {item.color} {item.brand}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </Stack>
+        <Stack
+          direction={"row"}
+          justifyContent="space-between"
+          sx={{ marginTop: 2, marginBottom: 1 }}
+        >
+          <strong>Entrega Aprox</strong>
+          <span>
+            {moment(order.delivery_date).format("MMMM d YYYY, h:mm a")}
+          </span>
+        </Stack>
+        <Stack direction={"column"} justifyContent="space-between">
+          {" "}
+          <strong>Observaciones:</strong>
+          {order.garments.map((item) => {
+            if (item.comment === "" && item.defects === "") return <></>;
+            return (
+              <span style={{ marginBottom: 5 }}>
+                {item.category} {item.gendre} {item.color} {item.brand}:{" "}
+                {item.comment} {item.defects}
+              </span>
+            );
+          })}
+        </Stack>
+      </div>
+    </Container>
   );
 });
